ce11b3cf9dd6e9985c7eeffd240f2a81de8ecca1
Calvin Gutierrez <calvinraphaelg@gmail.com>
2020-06-06 22:46:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,4 +4,8 @@
 #   "Amanda" => 27
 # }
 
-"Happy Birthday #{kids_name}! You are now #{age} years old!"
+def happy_birthday(birthday_kids)
+  birthday_kids.each do | kids_name , age  |
+    p "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
